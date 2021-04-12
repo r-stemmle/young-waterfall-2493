698b0fbaf09c846b7b1eac61ada5fe6f2ff04849
r-stemmle <ryan.stemmle@gmail.com>
2021-04-12 11:39:31 -0400

    Add movie titles to studio show page

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Studio Show Page" do
   before {
     @mgm = Studio.create!(name: "MGM", location: "Hollywood")
+    @top_gun = @mgm.movies.create!(title: "Top Gun", creation_year: 1985, genre: "action")
   }
 
   context "When I visit a studio show page" do
@@ -11,6 +12,12 @@ RSpec.describe "Studio Show Page" do
 
       expect(page).to have_content(@mgm.name)
       expect(page).to have_content(@mgm.location)
+    end
+
+    it "And I see the titles of all its movies" do
+      visit studio_path(@mgm)
+
+      expect(page).to have_content("Top Gun")
     end
   end
 end
